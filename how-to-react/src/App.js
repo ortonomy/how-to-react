15b8ef70f72d14323c351ffa85c5a20d88e35cc9
ortonomy <git@ortonomy.co>
2018-03-21 13:49:25 +0800

    makes app api driven using axios

--- how-to-react/src/App.js
+++ how-to-react/src/App.js
@@ -1,34 +1,61 @@
 // library dependencies
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 // component dependencies
 import Table from './components/Table';
 import TableRow from './components/TableRow';
 
-// data
-import Movies from './api/mock.js'
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      loading: true,
+      imageUrl: null
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('https://api.themoviedb.org/3/discover/movie?api_key=6f2b8b61c03afbeccc25962cf9ed8f5b&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2018')
+    .then ( res => {
+      this.setState({ movies: res.data.results })
+    })
+    .then ( () => {
+      Axios.get('https://api.themoviedb.org/3/configuration?api_key=6f2b8b61c03afbeccc25962cf9ed8f5b')
+      .then ( res => {
+        this.setState({imageUrl: res.data.images.base_url + res.data.images.poster_sizes[0], loading: false }) 
+      })
+    })
+    .catch ( err => ( console.log(err.message) ));
+  }
+
   render() {
+    const { movies, loading, imageUrl } = this.state;
+    if ( !loading ) {
+      return (
+        <Table>
+          {
+            movies.map( el => {
+              return (
+                <TableRow  
+                  key={el.title}
+                  posterPath={ imageUrl +  el.poster_path}
+                  title={el.title}
+                  overview={el.overview}
+                  language={el.language}
+                  releaseDate={el.release_date}
+                  popularity={el.popularity}
+                />
+              )
+            })
+          }
+        </Table>
+      );
+    }
     return (
-      <Table>
-        {
-          Movies.map( el => {
-            return (
-              <TableRow  
-                key={el.title}
-                posterPath={el.poster_path}
-                title={el.title}
-                overview={el.overview}
-                language={el.language}
-                releaseDate={el.release_date}
-                popularity={el.popularity}
-              />
-            )
-          })
-        }
-      </Table>
-    );
+      <div>LOADING</div>
+    )
   }
 }
 
